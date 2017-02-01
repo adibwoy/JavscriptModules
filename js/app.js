0b891bbe05cf8a47ab348950dfe88efb71d1be10
Aditya Bhandari <adityab@vmware.com>
2017-02-01 08:39:05 -0800

    Revealing Module Pattern
    Signed-off-by: Aditya Bhandari <adityab@vmware.com>

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 (function() {
 
-    console.log("Starting MultiMath with an IIFE");
+    console.log('Starting MultiMath with an IIFE');
 
-    document.getElementById("startGame").addEventListener("click", function() {
-
+    // add click handler to the start game button
+    document.getElementById('startGame').addEventListener('click', function() {
+        player.setName(document.getElementById('playername').value);
+        game.printGame();
     });
 
-    document.getElementById("calculate").addEventListener("click", function() {
-
+    // add click handler to the calculate score button
+    document.getElementById('calculate').addEventListener('click', function() {
+        game.calculateScore();
     });
 
-}());
+    // set the default number of problems
+    document.getElementById('problemCount').value = game.getProblemCount();
+
+})();
