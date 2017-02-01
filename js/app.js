e172805f419c8b4b0063ebccde6f60a917530cbd
Aditya Bhandari <adityab@vmware.com>
2017-02-01 08:07:15 -0800

    Creating a Test IIFE

---
+++ js/app.js
@@ -0,0 +1,13 @@
+(function() {
+
+    console.log("Starting MultiMath with an IIFE");
+
+    document.getElementById("startGame").addEventListener("click", function() {
+
+    });
+
+    document.getElementById("calculate").addEventListener("click", function() {
+
+    });
+
+}());
